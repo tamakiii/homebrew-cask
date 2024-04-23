0887d73e807f69a8e6ed58456cc0a2cfdda30a0a
tamakiii <tamakiii@users.noreply.github.com>
2024-04-23 15:39:13 +0900

    replace version with #{version}

--- Casks/kensington-trackball-works.rb
+++ Casks/kensington-trackball-works.rb
@@ -2,7 +2,7 @@ cask "kensington-trackball-works" do
   version "3.1.10"
   sha256 "1fe6c19c74d75c7b5263975502218b991bbd199254ee77df5062bd4bb76102f2"
 
-  url "https://www.kensington.com/siteassets/software-support/kensingtonworks/2024-update/kensingtonworks_3.1.10_1706808646.pkg"
+  url "https://www.kensington.com/siteassets/software-support/kensingtonworks/2024-update/kensingtonworks_#{version}_1706808646.pkg"
   name "Kensington Trackball Works"
   desc "Software driver for Kensington Trackball"
   homepage "https://www.kensington.com/software/kensingtonworks-customization-software/"
