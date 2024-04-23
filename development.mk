e6281d47af7bebd284d363d6cb91a6f688c9c3bf
tamakiii <tamakiii@users.noreply.github.com>
2024-04-23 15:15:55 +0900

    add snippet to get sha256

--- development.mk
+++ development.mk
@@ -1,6 +1,15 @@
-.PHONY: all install
+.PHONY: all install sha256
 
 all: install
 
 install:
 	brew install --cask Casks/kensington-trackball-works.rb
+
+sha256: tmp/kensingtonworks_3.1.10_1706808646.pkg
+	shasum -a 256 kensingtonworks_3.1.10_1706808646.pkg | awk '{print $$1}'
+
+tmp/kensingtonworks_3.1.10_1706808646.pkg: tmp
+	curl -so $@ https://www.kensington.com/siteassets/software-support/kensingtonworks/2024-update/kensingtonworks_3.1.10_1706808646.pkg
+
+tmp:
+	mkdir -p $@
